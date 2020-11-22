df2e6771ff6b020d43d86e7f95c30583f06243df
haxorsprogramming <haxorsuinsu@gmail.com>
2020-11-23 05:54:44 +0700

    update

--- platforms/browser/www/ladun/js/nadha-school-login.js
+++ platforms/browser/www/ladun/js/nadha-school-login.js
@@ -1,12 +1,21 @@
+// ROUTE 
+
+// VUE OBJECT
 var loginHand = new Vue({
     el : '#app',
     data : {
-
+        email : '',
+        password : ''
     },
     methods : {
         loginAtc : function()
         {
-            console.log("Fahrizan ...");
+            let email = document.querySelector('#txtEmail').value;
+            let password = document.querySelector('#txtPassword').value;
+            window.location.assign('mainApp/dasbor.html');
         }
     }
 });
+
+// INISIALISASI 
+document.querySelector('#txtEmail').focus();
