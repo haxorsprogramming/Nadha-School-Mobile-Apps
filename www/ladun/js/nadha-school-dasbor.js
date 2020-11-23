f624c82487b369df69533ed90d05bd0f4b7d0d7f
haxorsprogramming <haxorsuinsu@gmail.com>
2020-11-23 09:05:14 +0700

    update

--- www/ladun/js/nadha-school-dasbor.js
+++ www/ladun/js/nadha-school-dasbor.js
@@ -1,7 +1,11 @@
 var dasbor = new Vue({
     el : '#app',
     data : {
-
+        berita : [
+            { judul : 'Pelaksanaan Ukm' },
+            { judul : 'Dampak corona' },
+            { judul : 'Kursi itu' }
+        ]
     },
     methods : {
         homeAtc : function ()
@@ -13,6 +17,49 @@ var dasbor = new Vue({
         {
             $('.tab-link').removeClass('tab-link-active');
             $('#btbShop').addClass('tab-link-active');
+            
+        },
+        matkulAtc : function ()
+        {
+            console.log(Notification.permission);
+            if (Notification.permission === "granted") {
+                showNotification();
+            } else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(permission => {
+                    console.log(permission);
+                });
+            }
+        },
+        logOutAtc : function ()
+        {
+            logOut();
         }
     }
 });
+
+function pesanUmumApp(icon, title, text){
+    Swal.fire({icon : icon, title : title, text : text});
+}
+
+function showNotification() {
+    const notification = new Notification("New message incoming", {
+       body: "Hi there. How are you doing?",
+       icon: "yourimageurl.png"
+    })
+ }
+
+function logOut()
+{
+    Swal.fire({
+        title: "Logout?",
+        text: "Yakin keluar dari aplikasi ?...",
+        icon: "info",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Ya",
+        cancelButtonText: "Tidak",
+    }).then((result) => {
+        window.location.assign('../index.html');
+    });
+}
